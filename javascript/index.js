9b741bc6cfc95b5a8d91bdd688f7eb3fca997032
khadijah <karmayauabubakar@gmail.com>
2023-09-19 12:17:52 +0100

    update

--- javascript/index.js
+++ javascript/index.js
@@ -12,8 +12,8 @@ dropzone.addEventListener('drop', (e) => {
         if (file.type.match('image/jpeg') || file.type.match('image/jpg') || file.type.match('image/png')) {
             const img = document.createElement('img');
             img.setAttribute('draggable', 'false');
-            img.style.maxWidth = '12rem';
-            img.style.maxHeight = '6rem';
+            // img.style.maxWidth = '12rem';
+            // img.style.maxHeight = '6rem';
 
             const reader = new FileReader();
             reader.onload = (event) => {
@@ -22,8 +22,124 @@ dropzone.addEventListener('drop', (e) => {
             reader.readAsDataURL(file);
             dropzone.innerHTML = '';
             dropzone.appendChild(img);
+            newProperty.appendChild(img);
+            img.style.borderRadius = '1rem 1rem 0rem 0rem';
+            img.className = 'pics';
+
         } else {
             alert('Please drop a valid image (png, jpeg, jpg).');
         }
     }
 });
+
+// FORM
+const item_address = document.querySelector('.address');
+const room_types = document.querySelector('#room-type');
+const item_price = document.querySelector('.price');
+const add_btn = document.querySelector('.propertybtn');
+// const propertyItemsContainer = document.querySelector('.property-items'); // Container div
+
+// Get the select element
+add_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newProperty = document.createElement('div');
+    newProperty.className = 'one';
+
+    // Create a new div for each card
+    const location = document.createElement('h5');
+    const locationtVal = document.createTextNode(item_address.value);
+    location.appendChild(locationtVal);
+    newProperty.appendChild(location);
+
+    // Get the value of the selected option from the dropdown
+    const selectedRoom = document.createElement('h6');
+    const selectedRoomText = document.createTextNode(room_types.value);
+    selectedRoom.appendChild(selectedRoomText);
+    newProperty.appendChild(selectedRoom);
+
+    const priceValue = document.createElement('h4');
+    const priceValueText = document.createTextNode(`$ ${item_price.value}/month`);
+    priceValue.appendChild(priceValueText);
+    newProperty.appendChild(priceValue);
+
+    const propertytable = document.createElement('table');
+    propertytable.className = 'property-icons';
+    propertytable.innerHTML = `
+    <tbody>
+        <tr>
+            <td><img src="./icons/Bed.svg" alt="">4</td>
+            <td class="tdleft"><img src="./icons/Shower.svg" alt="">3</td>
+            <td class="tdleft"><img src="./icons/Size.svg" alt="">2</td>
+        </tr>
+    </tbody>
+`;
+    newProperty.appendChild(propertytable);
+    const propertyResult = document.querySelector('address');
+    propertyResult.appendChild(newProperty);
+    // propertyItemsContainer.appendChild(propertyResult)
+});
+
+// console.log('my name is deeyj')
+
+const propertyItems = document.querySelectorAll('.property-items .one');
+const propertyBtns = document.querySelectorAll('.property-btn button');
+const itemsPerPage = 6;
+let currentPage = 1;
+
+function showPage(pageNumber) {
+    const startIndex = (pageNumber - 1) * itemsPerPage;
+    const endIndex = pageNumber * itemsPerPage;
+
+    propertyItems.forEach((item, index) => {
+        if (index >= startIndex && index < endIndex) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+
+    // Update the active button
+    propertyBtns.forEach((btn) => {
+        btn.classList.remove('active');
+    });
+    propertyBtns[pageNumber].classList.add('active');
+}
+
+function updatePagination() {
+    const pageCount = Math.ceil(propertyItems.length / itemsPerPage);
+    showPage(currentPage);
+
+    for (let i = 1; i <= pageCount; i++) {
+        propertyBtns[i].style.display = 'inline-block';
+    }
+
+    // Hide or show Previous and Next buttons
+    if (currentPage === 1) {
+        document.getElementById('btn-1').style.display = 'none';
+    } else {
+        document.getElementById('btn-1').style.display = 'inline-block';
+    }
+    if (currentPage === pageCount) {
+        document.getElementById('btn-5').style.display = 'none';
+    } else {
+        document.getElementById('btn-5').style.display = 'inline-block';
+    }
+}
+// Add event listeners to pagination buttons
+propertyBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        if (index === 0) {
+            // Previous button
+            currentPage--;
+        } else if (index === propertyBtns.length - 1) {
+            // Next button
+            currentPage++;
+        } else {
+            currentPage = index;
+        }
+        updatePagination();
+    });
+});
+
+// Initialize pagination
+updatePagination();
